12bed18105258c3ae00abd3cd82ca7e43b6c7c59
Larysa Cohee <larysa.cohee@flatironschool.com>
2019-09-26 20:43:18 -0700

    working on auth

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,40 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { api } from '../services/api';
+
 
 class Login extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+          error: false,
+          fields: {
+            name: '',
+            password: ''
+          }
+        };
+      }
+    
+      handleChange = e => {
+        const newFields = { ...this.state.fields, [e.target.name]: e.target.value };
+        this.setState({ fields: newFields });
+      };
+    
+      handleSubmit = e => {
+        e.preventDefault();
+        api.auth.login(this.state.fields).then(res => {
+          if (!res.error) {
+            const updatedState = { ...this.state.auth, investor: res };
+            this.props.handleLogin(res);
+            this.props.history.push('/');
+          } else {
+            this.setState({ error: true });
+          }
+        });
+      };
+
     render() {
+        const { fields } = this.state;
         return (
             <>
                 <div className="login-page">
@@ -26,15 +58,15 @@ class Login extends React.Component {
             <div className="content">
               <form method="get" className="form-validate mb-4">
                 <div className="form-group">
-                  <input id="login-username" type="text" name="loginUsername" required data-msg="Please enter your username" className="input-material" />
+                  <input id="login-username" type="text" name="loginUsername" required data-msg="Please enter your username" className="input-material" value={fields.username} onChange={this.handleChange}/>
                   <label htmlFor="login-username" className="label-material">User Name</label>
                 </div>
                 <div className="form-group">
-                  <input id="login-password" type="password" name="loginPassword" required data-msg="Please enter your password" className="input-material" />
+                  <input id="login-password" type="password" name="loginPassword" required data-msg="Please enter your password" className="input-material" value={fields.password} onChange={this.handleChange}/>
                   <label htmlFor="login-password" className="label-material">Password</label>
                 </div>
                 <button type="submit" className="btn btn-primary">Login</button>
-              </form><a href="#" className="forgot-pass">Forgot Password?</a><br /><small>Do not have an account? </small><Link to="/signup" className="signup">Signup</Link>
+              </form><a href="#" className="forgot-pass">Forgot Password? (does not work yet)</a><br /><small>Do not have an account? </small><Link to="/signup" className="signup">Signup</Link>
             </div>
           </div>
         </div>
