70a85d3965627a78d03a86b0a6623900a5979f3b
Seanhetzel <seanhetzel1@gmail.com>
2019-12-18 18:04:27 -0800

    switched to hash router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-// import $ from 'jquery'
-// import 'jquery-validation'
 import React from "react";
 import {
-    BrowserRouter as Router,
+    HashRouter as Router,
     Route,
     Switch,
     Redirect
